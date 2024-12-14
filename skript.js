cbf1106c2f5794d6f53525fd0f97292ebc4c5107
islomking <yesuzb000@gmail.com>
2024-12-14 18:45:59 +0500

    Add files via upload

---
+++ skript.js
@@ -0,0 +1,16 @@
+ let userName = prompt('your name:');
+ 
+ let surName = prompt('your surname:');
+
+ let userAge = prompt('your age:');
+
+ let useIT = prompt('your IT:');
+
+ let useLocation = prompt('where do you live:');
+
+ alert(`Thank you \n ${userName}`);  
+
+ console.log(`Твоё имя ${userName}`)
+ console.log(`Твоя фамилия ${surName}`)
+ console.log(`Твой возраст ${userAge}`)
+ ``
